aae16e46ddc5b80414cb61227c5a476fd971cbe5
Cian <44465835+cian-hackathon@users.noreply.github.com>
2018-10-31 10:57:04 +0000

    Use callbacks instead of waiting for futures on every loop

--- src/main/java/UnicornPublisher.java
+++ src/main/java/UnicornPublisher.java
@@ -1,5 +1,6 @@
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.api.core.ApiFuture;
+import com.google.api.core.ApiFutureCallback;
 import com.google.api.core.ApiFutures;
 import com.google.cloud.ServiceOptions;
 import com.google.cloud.pubsub.v1.Publisher;
@@ -7,23 +8,25 @@ import com.google.protobuf.ByteString;
 import com.google.pubsub.v1.ProjectTopicName;
 import com.google.pubsub.v1.PubsubMessage;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+
+import static com.google.common.util.concurrent.MoreExecutors.directExecutor;
 
 public class UnicornPublisher {
 
     // use the default project id
     private static final String PROJECT_ID = ServiceOptions.getDefaultProjectId();
 
-    /** Publish messages to a topic.
+    /**
+     * Publish messages to a topic.
+     *
      * @param args topic name, number of messages
      */
     public static void main(String[] args) throws Exception {
         String topicId = "unicornTopic";
 
         ProjectTopicName topicName = ProjectTopicName.of(PROJECT_ID, topicId);
-        List<ApiFuture<String>> futures = new ArrayList<>();
 
         ObjectMapper mapper = new ObjectMapper();
 
@@ -34,30 +37,29 @@ public class UnicornPublisher {
 
         Publisher publisher = Publisher.newBuilder(topicName).build();
 
-        while(true) {
-            try {
-                // Create a publisher instance with default settings bound to the topic
-                for (Unicorn unicorn : unicorns) {
-                    unicorn.move();
-                    // convert message to bytes
-                    ByteString data = ByteString.copyFromUtf8(mapper.writeValueAsString(unicorn));
-                    PubsubMessage pubsubMessage = PubsubMessage.newBuilder()
-                        .setData(data)
-                        .build();
+        while (true) {
+            // Create a publisher instance with default settings bound to the topic
+            for (Unicorn unicorn : unicorns) {
+                unicorn.move();
+                // convert message to bytes
+                ByteString data = ByteString.copyFromUtf8(mapper.writeValueAsString(unicorn));
+                PubsubMessage pubsubMessage = PubsubMessage.newBuilder()
+                    .setData(data)
+                    .build();
 
-                    // Schedule a message to be published. Messages are automatically batched.
-                    ApiFuture<String> future = publisher.publish(pubsubMessage);
-                    futures.add(future);
-                    Thread.sleep(5000);
-                }
-            } finally {
-                // Wait on any pending requests
-                List<String> messageIds = ApiFutures.allAsList(futures).get();
-                futures.clear();
+                // Schedule a message to be published. Messages are automatically batched.
+                ApiFuture<String> future = publisher.publish(pubsubMessage);
+                ApiFutures.addCallback(future, new ApiFutureCallback<String>() {
+                    public void onSuccess(String messageId) {
+                        System.out.println("Published with message id: " + messageId);
+                    }
 
-                for (String messageId : messageIds) {
-                    System.out.println(messageId);
-                }
+                    public void onFailure(Throwable t) {
+                        System.out.println("Failed to publish: " + t);
+                    }
+                }, directExecutor());
+
+                Thread.sleep(5000);
             }
         }
     }
