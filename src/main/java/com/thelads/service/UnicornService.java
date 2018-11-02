eb8dcd3a6c403e226797d0624a84b155c1316feb
Gareth Rothwell <gareth.rothwell@equifax.com>
2018-11-02 11:13:43 +0000

    Update datastore logic and de-duplication

--- src/main/java/com/thelads/service/UnicornService.java
+++ src/main/java/com/thelads/service/UnicornService.java
@@ -21,18 +21,29 @@ public class UnicornService {
 
         datastoreQuery = new DatastoreQuery();
         QueryResults<Entity> allTasks = datastoreQuery.listTasks();
+
         List<Unicorn> unicornList = new ArrayList<>();
+        List<Entity> entityList = new ArrayList<>();
+        List<String> distinctNames = new ArrayList<>();
+
+
         allTasks.forEachRemaining(entity ->
         {
-            unicornList.add(new Unicorn.Builder()
-                .setName(entity.getValue("name").get().toString())
-                .setDistance(Double.valueOf(entity.getValue("distance").get().toString()))
-                .setLatitude(Double.valueOf(entity.getValue("latitude").get().toString()))
-                .setLongitude(Double.valueOf(entity.getValue("longitude").get().toString()))
-                .setStatusTime(LocalDateTime.parse(entity.getValue("statusTime").get().toString(), formatter).toString())
-                .setHealthPoints(Integer.valueOf(entity.getValue("healthPoints").get().toString()))
-                .setMagicPoints(Integer.valueOf(entity.getValue("magicPoints").get().toString())).build());
+            if(!distinctNames.contains(entity.getValue("name").get().toString())){
+                distinctNames.add(entity.getValue("name").get().toString());
+                entityList.add(entity);
+            }
         });
+
+        entityList.forEach(entity -> unicornList.add(new Unicorn.Builder()
+            .setName(entity.getValue("name").get().toString())
+            .setDistance(Double.valueOf(entity.getValue("distance").get().toString()))
+            .setLatitude(Double.valueOf(entity.getValue("latitude").get().toString()))
+            .setLongitude(Double.valueOf(entity.getValue("longitude").get().toString()))
+            .setStatusTime(LocalDateTime.parse(entity.getValue("statusTime").get().toString(), formatter).toString())
+            .setHealthPoints(Integer.valueOf(entity.getValue("healthPoints").get().toString()))
+            .setMagicPoints(Integer.valueOf(entity.getValue("magicPoints").get().toString())).build()));
+
         return unicornList;
     }
 }
