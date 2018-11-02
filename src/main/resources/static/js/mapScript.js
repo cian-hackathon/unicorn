a329398c0ee5f9b4f4089cd960b73b87c0920fbf
Cian <44465835+cian-hackathon@users.noreply.github.com>
2018-11-02 16:32:01 +0000

    Update frontend to new design

--- src/main/resources/static/js/mapScript.js
+++ src/main/resources/static/js/mapScript.js
@@ -91,3 +91,39 @@ $("#next").on("click", function() {
     });
     return false; // this one
 });
+
+window.onload = function () {
+
+    var l = Snap('#logo');
+
+    setTimeout( function() {
+        // modify this one line below, and see the result !
+        var logoTitle = 'ubercorn';
+        var logoRandom = '';
+        var logoTitleContainer = l.text(0, '98%', '');
+        var possible = "-+*/|}{[]~\\\":;?/.><=+-_)(*&^%$#@!)}";
+        logoTitleContainer.attr({
+            fontSize: 280,
+            fontFamily: 'Metrophobic',
+            fontWeight: '600',
+            fill: "#fff0e3"
+        });
+
+        function generateRandomTitle(i, logoRandom) {
+            setTimeout( function() {
+                logoTitleContainer.attr({ text: logoRandom });
+            }, i*70 );
+        }
+
+        for( var i=0; i < logoTitle.length+1; i++ ) {
+            logoRandom = logoTitle.substr(0, i);
+            for( var j=i; j < logoTitle.length; j++ ) {
+                logoRandom += possible.charAt(Math.floor(Math.random() * possible.length));
+            }
+            generateRandomTitle(i, logoRandom);
+            logoRandom = '';
+        }
+
+    }, 500 );
+};
+
