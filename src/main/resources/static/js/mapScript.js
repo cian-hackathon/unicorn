a98f8d3484980991f24f4338426b33bee9de3916
Cian <44465835+cian-hackathon@users.noreply.github.com>
2018-11-05 11:22:36 +0000

    Remove delay from title animation function

--- src/main/resources/static/js/mapScript.js
+++ src/main/resources/static/js/mapScript.js
@@ -117,35 +117,31 @@ window.onload = function () {
 
     var l = Snap('#logo');
 
-    setTimeout( function() {
-        // modify this one line below, and see the result !
-        var logoTitle = 'ubercorn';
-        var logoRandom = '';
-        var logoTitleContainer = l.text(0, '98%', '');
-        var possible = "-+*/|}{[]~\\\":;?/.><=+-_)(*&^%$#@!)}";
-        logoTitleContainer.attr({
-            fontSize: 280,
-            fontFamily: 'Metrophobic',
-            fontWeight: '600',
-            fill: "#fff0e3"
-        });
+    var logoTitle = 'ubercorn';
+    var logoRandom = '';
+    var logoTitleContainer = l.text(0, '98%', '');
+    var possible = "-+*/|}{[]~\\\":;?/.><=+-_)(*&^%$#@!)}";
+    logoTitleContainer.attr({
+        fontSize: 280,
+        fontFamily: 'Metrophobic',
+        fontWeight: '600',
+        fill: "#fff0e3"
+    });
 
-        function generateRandomTitle(i, logoRandom) {
-            setTimeout( function() {
-                logoTitleContainer.attr({ text: logoRandom });
-            }, i*70 );
+    function generateRandomTitle(i, logoRandom) {
+        setTimeout( function() {
+            logoTitleContainer.attr({ text: logoRandom });
+        }, i*70 );
+    }
+
+    for( var i=0; i < logoTitle.length+1; i++ ) {
+        logoRandom = logoTitle.substr(0, i);
+        for( var j=i; j < logoTitle.length; j++ ) {
+            logoRandom += possible.charAt(Math.floor(Math.random() * possible.length));
         }
-
-        for( var i=0; i < logoTitle.length+1; i++ ) {
-            logoRandom = logoTitle.substr(0, i);
-            for( var j=i; j < logoTitle.length; j++ ) {
-                logoRandom += possible.charAt(Math.floor(Math.random() * possible.length));
-            }
-            generateRandomTitle(i, logoRandom);
-            logoRandom = '';
-        }
-
-    }, 500 );
+        generateRandomTitle(i, logoRandom);
+        logoRandom = '';
+    }
 };
 
 /*
